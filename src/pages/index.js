fcc55419256bf578a8b117fc644a63f7e3596fd9
ljaviertovar <luisjavier_tovar@outlook.com>
2023-02-17 19:33:11 -0500

    update client side

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,19 @@
 import { useEffect, useRef, useState } from 'react'
-import { ApolloClient, InMemoryCache, createHttpLink, useLazyQuery, useQuery } from '@apollo/client'
+import { useLazyQuery, useQuery } from '@apollo/client'
 
 import Head from 'next/head'
-import { Button, Container, Grid, Input, Spacer, User, Row } from "@nextui-org/react"
+import { Button, Container, Grid, Input, Spacer, User, Row, Loading } from "@nextui-org/react"
 
 import GET_USERS from '@/graphql/queries/getUsers.gql'
 import SEARCH_USERS from '@/graphql/queries/searchUsers.gql'
 
-import { request } from 'graphql-request'
-
-import { gql } from 'apollo-server-core'
-
-import client from '@/graphql/apollo-client'
-
-export default function Home({ staticUsers = [] }) {
-
-  const [users, setUsers] = useState(staticUsers)
+export default function Home() {
+  const [users, setUsers] = useState([])
   const [searchValue, setSearchValue] = useState('')
 
-  const staticUSersRef = useRef(staticUsers)
+  const usersRef = useRef(null)
 
-  // const { data } = useQuery(GET_USERS)
+  const { data, loading, error } = useQuery(GET_USERS)
 
   const [getSearchedUsers] = useLazyQuery(SEARCH_USERS, {
     fetchPolicy: 'network-only',
@@ -28,6 +21,14 @@ export default function Home({ staticUsers = [] }) {
       setUsers(data.searchUser)
     }
   })
+
+  useEffect(() => {
+    if (data) {
+      setUsers(data.users)
+      usersRef.current = data.users
+    }
+  }, [data])
+
 
   const searchUser = () => {
     getSearchedUsers({
@@ -37,13 +38,10 @@ export default function Home({ staticUsers = [] }) {
     })
   }
 
-  // useEffect(() => {
-
-  //   if (data) {
-  //     setUsers(data.users)
-  //   }
-  // }, [data])
-
+  if (error) {
+    console.error(error)
+    return null
+  }
 
   return (
     <>
@@ -62,7 +60,7 @@ export default function Home({ staticUsers = [] }) {
             <Input
               clearable
               labelPlaceholder="User"
-              onClearClick={() => setUsers(staticUSersRef.current)}
+              onClearClick={() => setUsers(usersRef.current)}
               initialValue={searchValue}
               onChange={(e) => setSearchValue(e.target.value)}
             />
@@ -74,114 +72,33 @@ export default function Home({ staticUsers = [] }) {
           <Spacer y={2.5} />
           <Row justify="center" align="center">
 
-            <Grid.Container gap={2} justify="center">
-              {users.map(u => (
-                <Grid xs={3}
-                  key={u.id}
-                >
-                  <User
-                    src={u.image}
-                    name={`${u.firstName}${u.lastName}`}
-                    description={u.email}
-                    size="lg"
-                    bordered
-                    color="gradient"
-                  />
-                </Grid>
+            {loading
+              ?
+              <Loading />
+              :
+              <Grid.Container gap={2} justify="center">
+                {users.map(u => (
+                  <Grid xs={3}
+                    key={u.id}
+                  >
+                    <User
+                      src={u.image}
+                      name={`${u.firstName}${u.lastName}`}
+                      description={u.email}
+                      size="lg"
+                      bordered
+                      color="gradient"
+                    />
+                  </Grid>
 
-              ))
-              }
-            </Grid.Container>
+                ))
+                }
+              </Grid.Container>
+            }
+
           </Row>
         </Container>
       </main>
     </>
   )
-}
-
-export const getServerSideProps = async () => {
-
-  // let results = await fetch('http://localhost:3000/api/graphql', {
-  //   method: 'POST',
-
-  //   headers: {
-  //     "Content-Type": "application/json"
-  //   },
-
-  //   body: JSON.stringify({
-  //     query: `{
-  //       users {
-  //         		id
-  //         		firstName
-  //         		lastName
-  //         		email
-  //         		username
-  //         		image
-  //         	}
-  //     }`
-  //   })
-  // })
-  // let { data } = await results.json()
-
-  // console.log('DATA', data)
-  // let results = await fetch('https://rickandmortyapi.com/graphql', {
-  //   method: 'POST',
-
-  //   headers: {
-  //     "Content-Type": "application/json"
-  //   },
-
-  //   body: JSON.stringify({
-  //     query: `{
-  //       characters {
-  //         results {
-  //           name
-  //         }
-  //       }
-  //     }`
-  //   })
-  // })
-  // let characters = await results.json()
-  // console.log(characters.data)
-
-  // return {
-  //   props: {
-  //     characters: characters.data
-  //     // staticUsers: data.users
-  //     // staticUsers: []
-  //   }
-  // }
-
-  //   const query = `
-  //   query {
-  //     users {
-  //       id
-  //       firstName
-  //       lastName
-  //       email
-  //       image
-  //     }
-  //   }
-  // `
-  //   const data = await request('http://localhost:3000/api/graphql', query)
-
-  const client = new ApolloClient({
-    ssrMode: true,
-    link: createHttpLink({
-      uri: 'http://localhost/api/graphql',
-      credentials: 'same-origin',
-      headers: {
-        'Access-Control-Allow-Credentials': true,
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET,OPTIONS,PATCH,DELETE,POST,PUT'
-      },
-    }),
-    cache: new InMemoryCache(),
-  })
-
-
-
-  const { data } = await client.query({ query: GET_USERS })
-
-  return { props: { staticUsers: data.users } }
 }
