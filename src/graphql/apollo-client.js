dc7c0ce6eb64cbd8ef3d610ab0be1ba8163b7226
ljaviertovar <luisjavier_tovar@outlook.com>
2023-02-19 13:33:34 -0500

    update home

--- src/graphql/apollo-client.js
+++ src/graphql/apollo-client.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client"
 
 const client = new ApolloClient({
-  uri: process.env.NEXT_PUBLIC_URL_SERVER_GRAPHQL,
+  uri: 'http://localhost:3000/api/graphql',
   cache: new InMemoryCache(),
 })
 
