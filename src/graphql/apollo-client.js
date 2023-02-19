e0cd783ab2645b85cf89a9b65ddc026ce355a2f0
ljaviertovar <luisjavier_tovar@outlook.com>
2023-02-19 14:02:44 -0500

    update cors

--- src/graphql/apollo-client.js
+++ src/graphql/apollo-client.js
@@ -1,23 +1,7 @@
-import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client'
-import { setContext } from '@apollo/client/link/context'
-
-const httpLink = createHttpLink({
-  uri: process.env.NEXT_PUBLIC_URL_SERVER_GRAPHQL,
-})
-
-const authLink = setContext((_, { headers }) => {
-  return {
-    headers: {
-      ...headers,
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Headers': 'Content-Type',
-      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
-    }
-  }
-})
+import { ApolloClient, InMemoryCache } from "@apollo/client"
 
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  uri: process.env.NEXT_PUBLIC_URL_SERVER_GRAPHQL,
   cache: new InMemoryCache(),
 })
 
